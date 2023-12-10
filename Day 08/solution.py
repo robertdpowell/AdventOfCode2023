c6c93a477cc96d389a5b6947b199a0e1512c3ddd
robertdpowell <robertdpowell@gmail.com>
2023-12-10 08:25:18 +0000

    Day 7, 8

--- Day 08/solution.py
+++ Day 08/solution.py
@@ -29,39 +29,4 @@ def solve1():
                         break
     return len(steps_to_zzz)
 
-
-def solve2():
-    for line in lines[2:]:
-        location = line.split('=')[0].strip()[-1]
-        left = line.split('(')[1].split(',')[0].strip()
-        right = line.split(')')[0].split(',')[1].strip()
-        nodes.append((location, left, right))
-    print (nodes)
-
-    cycle_lengths = []
-    for start_node in nodes:
-        if start_node[0] == 'A':
-            cycles = 0
-            current_position = start_node[0]
-
-            while current_position[0] != 'Z':
-                for instruction in instructions:
-                    if instruction == 'R':
-                        for node in nodes:
-                            if node[0] == current_position:
-                                current_position = node[2]
-                                cycles += 1
-                                print (cycles)
-                                break
-
-                    elif instruction == 'L':
-                        for node in nodes:
-                            if node[0] == current_position:
-                                current_position = node[1]
-                                cycles += 1
-                                print(cycles)
-                                break
-        cycle_lengths.append(cycles)
-    print (cycle_lengths)
-solve2()
-# print (f'solution 1 answer is {solve1()}')
+print (f'solution 1 answer is {solve1()}')
---
+++ readme.md
@@ -0,0 +1,64 @@
+**Day 1**
+
+Part 1: Complete
+
+Part 2: Complete
+
+---
+
+**Day 2**
+
+Part 1: Complete
+
+Part 2: Complete
+
+---
+
+**Day 3**
+
+Part 1: Complete
+
+---
+
+**Day 4**
+
+Part 1: Complete
+
+Part 2: Complete
+
+---
+
+**Day 5**
+
+---
+
+
+**Day 6**
+
+Part 1: Complete
+
+Part 2: Complete
+
+---
+
+**Day 7**
+
+Part 1: Complete
+
+Part 2: Complete
+
+---
+
+
+**Day 8**
+
+Part 1: Complete
+
+---
+
+**Day 9**
+
+---
+
+
+
